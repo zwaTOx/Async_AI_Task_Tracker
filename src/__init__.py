4ed8aa042448eb64136d64af9d1caa0c766eb2b7
zwaTOx <yakovva36@gmail.com>
2025-09-22 15:34:15 +0700

    add CORS

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from src.database import init_db
 from src.user.routes import user_router
@@ -14,6 +15,14 @@ async def lifespan(app: FastAPI):
 app = FastAPI(
     title='AI Task Traker',
     lifespan=lifespan
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"],
 )
 
 @app.get('/')
