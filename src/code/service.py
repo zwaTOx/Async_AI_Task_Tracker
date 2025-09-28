81b1025fb4403ab466229bc5565b18c6cf229ae9
zwaTOx <yakovva36@gmail.com>
2025-09-28 20:51:45 +0700

    FIX: multiple equal codes bug

--- src/code/service.py
+++ src/code/service.py
@@ -4,6 +4,7 @@ from src.code.repository import CodeRepository
 from src.email.password import send_recovery_code
 from src.exceptions import BadRequestException
 from src.user.repository import UserRepository
+from .utils import generate_code
 from .utils import create_reset_password_token, decode_reset_password_token
 
 class CodeService:
@@ -14,7 +15,8 @@ class CodeService:
         founded_user = await UserRepository(self.session).get_user_by_email(email)
         if founded_user is None:
             raise BadRequestException("Пользователя с таким email не существует")
-        code = send_recovery_code(email)
+        code = generate_code()
+        send_recovery_code(email, code)
         await CodeRepository(self.session).create_restore_code(founded_user.id, code)
         return code, founded_user.id
     
