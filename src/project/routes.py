364c3e552aa784d3b0bbcf33ebc4ca9ac07a30a2
zwaTOx <yakovva36@gmail.com>
2025-11-11 22:11:05 +0700

    add project role dep

--- src/project/routes.py
+++ src/project/routes.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends, status
 
 from src.database import DbSession
+from src.role.dependencies import can_delete_project, can_view_project, can_edit_project
 from src.user.dependencies import CurrentUser
 from .schemes import ProjectCreate, ProjectResponse, ProjectPagination, ProjectUpdate
 from .repository import ProjectRepository
@@ -39,16 +40,14 @@ async def create_project(
     return new_project
 
 @project_router.patch(
-    "/{project_id}",
-    dependencies=[
-        Depends(verify_project_action(Action.EDIT))
-    ]
+    "/{project_id}"
 )
 async def update_project(
     session: DbSession,
     user: CurrentUser,
     project_id: int,
-    project_update_data: ProjectUpdate
+    project_update_data: ProjectUpdate,
+    _ = Depends(can_edit_project())
 ):
     updated_project = await ProjectService(session).update_project(
         user.id, project_id, project_update_data
@@ -57,14 +56,12 @@ async def update_project(
 
 @project_router.delete(
     "/{project_id}",
-    status_code=status.HTTP_204_NO_CONTENT,
-    dependencies=[
-        Depends(verify_project_action(Action.DELETE))
-    ]
+    status_code=status.HTTP_204_NO_CONTENT
 )
 async def delete_project(
     session: DbSession,
     user: CurrentUser,
-    project_id: int
+    project_id: int,
+    _ = Depends(can_delete_project())
 ):
     await ProjectService(session).delete_project(user.id, project_id)
